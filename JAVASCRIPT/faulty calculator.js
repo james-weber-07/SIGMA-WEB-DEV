998fb65ca8a7d40f9cd88179cd504e33c678409f
Jayant <iamjayantsa@gmail.com>
2025-08-18 19:56:02 +0530

    ADDED
    ADDED

---
+++ JAVASCRIPT/faulty calculator.js
@@ -0,0 +1,26 @@
+console.log("HEY I AM JAYANT")
+let a=5
+let b=3
+result1=a+b
+result2=a-b
+result3=a*b
+result4=a/b
+RESULT1=a-b
+RESULT2=a/b
+RESULT3=a-b
+RESULT4=a%b
+let x=Math.random()
+if(x<0.1){
+       console.log(x)
+       console.log("THE SUM OF A AND B IS",result1)
+       console.log("THE DIFFERENCE OF A AND B IS",result2)
+       console.log("THE PRODUCT OF A AND B IS",result3)
+       console.log("THE DIVISION OF A AND B IS",result4)
+}
+else{
+              console.log(x)
+       console.log("THE SUM OF A AND B IS",RESULT1)
+       console.log("THE DIFFERENCE OF A AND B IS",RESULT2)
+       console.log("THE PRODUCT OF A AND B IS",RESULT3)
+       console.log("THE DIVISION OF A AND B IS",RESULT4)
+}
---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,14 @@
+console.log("I AM LEARNING JS")
+// var a=6;
+// var b=7;
+// console.log(a+b)
+// let a=7;
+// let b=6;
+// console.log(a+b)
+
+let a={
+      Name:"JAYANT",
+      "Roll No":14,
+      "Phone Number":"xxxxxx1609"
+}
+console.log(a)
