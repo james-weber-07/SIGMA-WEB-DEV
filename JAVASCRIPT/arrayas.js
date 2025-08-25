b0ffe41e311a3dee748066f8f95507a718e07501
Jayant <iamjayantsa@gmail.com>
2025-08-25 21:36:24 +0530

    added

---
+++ JAVASCRIPT/arrayas.js
@@ -0,0 +1,8 @@
+let a=[1,2,3]
+let b=[4,5,6]
+let c=[7,8,9]
+let d=a.concat(b,c)
+console.log(d)
+
+let x=[22,43,17,51,15,21]
+console.log(x.sort())
--- JAVASCRIPT/tempCodeRunnerFile.js
+++ JAVASCRIPT/tempCodeRunnerFile.js
@@ -1,13 +1,3 @@
-let a="jay\"ant"
-let b="jesta"
-console.log(`Hey my name is ${a} and my brother name is ${b}`)
-console.log(a.toUpperCase())
-console.log(a.concat(b," awate"))
-console.log(a.length)
-console.log(a.slice(2))
-console.log(a.replace("\"","y"))
-function harry(a,b){
- return  a+b;
-}
-console.log("the sum of a and b is "+ harry(1,2))
-console.log("my name is"+a)
+let g="BROS "
+let h="LIMITED "
+let i="HUB "
